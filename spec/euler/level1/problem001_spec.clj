1425c902a5dcfd45307e641bd8b0df0cc8175573
maniginam <maniginam@gmail.com>
2020-06-08 13:29:20 -0500

    solved #1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,23 @@
 
 (describe "Euler Problem #1"
 
+	(it "divisible by 3 or 5"
+		(should= true (multiple-of-3-or-5? 0))
+		(should= false (multiple-of-3-or-5? 1))
+		(should= false (multiple-of-3-or-5? 2))
+		(should= true (multiple-of-3-or-5? 3))
+		(should= false (multiple-of-3-or-5? 4))
+		(should= true (multiple-of-3-or-5? 5))
+		(should= true (multiple-of-3-or-5? 6))
+		(should= false (multiple-of-3-or-5? 7))
+		(should= false (multiple-of-3-or-5? 8))
+		(should= true (multiple-of-3-or-5? 9))
+	)
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+	(should= 78 (euler-1 20))
+	(should= 233168 (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,12 @@
-(ns euler.level1.problem001)
+(ns euler.level1.problem001
+	(:require [clojure.set]))
+
+
+(defn multiple-of-3-or-5? [n]
+	(or (integer? (/ n 3)) (integer? (/ n 5))))
+	
+	
 
 (defn euler-1 [n]
-  )
+	(let [multiples-of-3-or-5 (filter multiple-of-3-or-5? (range n))]
+		(apply + multiples-of-3-or-5)))
