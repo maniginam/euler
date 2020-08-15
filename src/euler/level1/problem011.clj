f12a271e4451fde227acb2c52774f579c5a9e049
maniginam <maniginam@gmail.com>
2020-08-15 13:56:29 -0500

    complete euler 11

--- src/euler/level1/problem011.clj
+++ src/euler/level1/problem011.clj
@@ -1,18 +1,40 @@
 (ns euler.level1.problem011)
 
+(defn get-values [grid row col]
+  (nth (nth grid row) col))
+
 (defn find-columns [grid col]
   (for [row (range (count grid))]
-    (nth (nth grid row) col)))
+    (get-values grid row col)))
+
+(defn lt-diag [grid column]
+    (loop [row 0
+         col column
+         diagonal []]
+    (if (>= col 0)
+      (recur (inc row) (dec col) (conj diagonal (get-values grid row col)))
+      diagonal)))
+
+(defn rt-diag [grid column]
+  (let [grid-size (count grid)]
+    (loop [row 0
+           col column
+           diagonal []]
+      (if (< col grid-size)
+        (recur (inc row) (inc col) (conj diagonal (get-values grid row col)))
+        diagonal))))
 
 (defn find-diagonals [grid]
-  (let [grid-size (count grid)]
-    (loop [x 1
-         y 0
-         diagonal []
-         diagonals []
-         count 0]
-    (if (and (= x grid-size) (= y grid-size))
-      diagonals)
+  (let [grid-size (count grid)
+        columns (range grid-size)
+        first-row-rt-diags (map #(rt-diag grid %) columns)
+        first-row-lt-diags (map #(lt-diag grid %) columns)
+        last-row-rt-diags (map #(rt-diag (reverse grid) %) columns)
+        last-row-lt-diags (map #(lt-diag (reverse grid) %) columns)
+        diagonals (mapcat #(conj [] %1 %2 %3 %4)
+                          first-row-lt-diags first-row-rt-diags
+                          last-row-lt-diags last-row-rt-diags)]
+    (remove #(< (count %) 4) diagonals)))
 
 (defn multiply [vector]
   (apply max
@@ -21,9 +43,9 @@
 
 (defn euler-11 [grid]
   (let [cols (for [col (range (count grid))] (find-columns grid col))
-        ;diagonals (find-diagonals grid)
+        diagonals (find-diagonals grid)
         max-row-product (apply max (for [row grid] (multiply row)))
-        max-col-product (apply max (for [col cols] (multiply col)))]
-    diagonals))
-;(max max-row-product max-col-product)))
+        max-col-product (apply max (for [col cols] (multiply col)))
+        max-diag-product (apply max (for [diag diagonals] (multiply diag)))]
+(max max-row-product max-col-product max-diag-product)))
 
